6c0a5b40fb3f0091797c56209fdfd34a805b5b01
NguyenMinhChien <minhchien03112003@gmail.com>
2023-07-13 19:25:37 +0700

    update code 3

---
+++ LinQGUI/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace LinQGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(72, 97);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(316, 325);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(419, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(316, 325);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ LinQGUI/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinQGUI
+{
+    public partial class Form1 : Form
+    {
+        List<Food> foodList;
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadFood();
+        }
+
+        void LoadFood()
+        {
+            foodList = new List<Food>();
+            foodList.Add(new Food("Com tam", "25000"));
+            foodList.Add(new Food("Bun Bo", "35000"));
+            foodList.Add(new Food("Bun Rieu", "30000"));
+            foodList.Add(new Food("Com ga", "27000"));
+            foodList.Add(new Food("Com suon", "25000"));
+
+            comboBox1.DataSource = foodList;
+            comboBox1.DisplayMember = "name";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Food> result = new List<Food>();
+
+            /*foreach(Food item in foodList)
+            {
+                if(item.name == textBox1.Text)
+                {
+                    result.Add(item);
+                }
+            }*/
+
+            //Su dung LinQ
+            result = foodList.Where(item => item.name == textBox1.Text).ToList();
+
+            comboBox2.DataSource = result;
+            comboBox2.DisplayMember = "name";
+        }
+    }
+
+    class Food
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+
+        public Food()
+        {
+
+        }
+
+        public Food(string n, string p)
+        {
+            this.name = n;
+            this.price = p;
+        }
+    }
+}
